8f2cc4ea9398f6ea181dd4fa6623196df19fdecc
Felipe Goes <fgoesoliveira@gmail.com>
2018-08-20 20:05:50 -0300

    [ADD] - Js final exercises

---
+++ javascript_2/js/objects.js
@@ -0,0 +1,5 @@
+var car_info = {year:1997,
+                manufac:"Peugeot",
+                engines:[1.8,2.0,1.6],
+                outros:{ar:"frio",portas:4}
+}
---
+++ javascript_2/js/objects_exercise.js
@@ -0,0 +1,8 @@
+var employee = {
+  name: "John Smith",
+  job: "Programmer",
+  age: 31,
+  name_lenght: function(){
+    alert(this.name.length)
+  }
+}
